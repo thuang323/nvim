689352d1012208741750bae6d52720615ba138eb
Taylor Huang <taylorhuang@taylors-mbp.mynetworksettings.com>
2023-12-20 22:29:20 -0500

    adds liveserver and creates its keymaps

---
+++ lua/taylor/plugins/live-server.lua
@@ -0,0 +1,13 @@
+return {
+	"aurum77/live-server.nvim",
+	build = function()
+		require("live_server.util").install()
+	end,
+	cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
+	config = function()
+		local live_server = require("live_server")
+		live_server.setup({
+			browser_command = "Google Chrome",
+		})
+	end,
+}
