d72331c78972772fea534f01cb90e954762b6f56
thuang323 <taylorhuang0323@gmail.com>
2024-04-13 01:51:38 -0400

    updates

--- lua/taylor/plugins/obsidian.lua
+++ lua/taylor/plugins/obsidian.lua
@@ -43,7 +43,8 @@ return {
 				-- 1. Whether to add the note ID during completion.
 				-- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
 				-- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-				prepend_note_id = true,
+				-- prepend_note_id = true,
+				prepend_note_id = false,
 				-- 2. Whether to add the note path during completion.
 				-- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
 				-- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
