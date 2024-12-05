ec07aca2ea89ec9935e456487023a588a18f77da
thuang323 <taylorhuang0323@gmail.com>
2024-12-05 16:26:55 -0500

    aa

--- temp.sh
+++ temp.sh
@@ -1,4 +1,4 @@
-git filter-repo --commit-callback '
+git filter-repo --force --commit-callback '
 commit.author_name = b"thuang323"
 commit.author_email = b"taylorhuang0323@gmail.com"
 commit.committer_name = b"thuang323"
