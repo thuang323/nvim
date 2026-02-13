45111f08fc9884eccbb9ff9879913289846d58a0
thuang323 <taylorhuang0323@gmail.com>
2026-02-13 18:15:07 -0500

    update

--- lsp/dartls.lua
+++ lsp/dartls.lua
@@ -1,34 +1,18 @@
 return {
-  cmd = { "dart", "language-server", "--protocol=lsp" },
-  filetypes = { "dart" },
-  init_options = {
-    onlyAnalyzeProjectsWithOpenFiles = true,
-    suggestFromUnimportedLibraries = true,
-    closingLabels = true,
-    outline = true,
-    flutterOutline = true,
-  },
-  root_markers = { "pubspec.yaml" },
-  settings = {
-    dart = {
-      completeFunctionCalls = true,
-      showTodos = true,
-    }
-  }
-  -- cmd = { 'dart', 'language-server', '--protocol=lsp' },
-  --   filetypes = { 'dart' },
-  --   root_dir = util.root_pattern 'pubspec.yaml',
-  --   init_options = {
-  --     onlyAnalyzeProjectsWithOpenFiles = true,
-  --     suggestFromUnimportedLibraries = true,
-  --     closingLabels = true,
-  --     outline = true,
-  --     flutterOutline = true,
-  --   },
-  --   settings = {
-  --     dart = {
-  --       completeFunctionCalls = true,
-  --       showTodos = true,
-  --     },
-  --   },
+	cmd = { "dart", "language-server", "--protocol=lsp" },
+	filetypes = { "dart" },
+	init_options = {
+		onlyAnalyzeProjectsWithOpenFiles = true,
+		suggestFromUnimportedLibraries = true,
+		closingLabels = true,
+		outline = true,
+		flutterOutline = true,
+	},
+	root_markers = { "pubspec.yaml" },
+	settings = {
+		dart = {
+			completeFunctionCalls = true,
+			showTodos = true,
+		},
+	},
 }
